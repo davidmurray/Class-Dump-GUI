2bb82f000cea327eaa64f14fd4f1dc24069ed835
Cykey <cykey@live.com>
2013-12-13 15:28:04 -0500

    Move CDGViewController in its own class.

---
+++ CDGViewController.h
@@ -0,0 +1,19 @@
+#import <UIKit/UIKit.h>
+#import <AppList.h>
+#import "UIProgressHUD.h"
+
+@interface CDGViewController : UITableViewController <UIAlertViewDelegate> {
+@private
+	ALApplicationTableDataSource *dataSource;
+    ALApplicationList *al;
+    UIProgressHUD *HUD;
+    NSString *appDisplayName;
+}
+
+-(void)getApp:(NSString *)bundleID;
+-(void)dumpBinaryAtPath:(NSString *)path binaryName:(NSString *)binaryName;
+-(void)dumpCustomBinary;
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex;
+
+@end
+
